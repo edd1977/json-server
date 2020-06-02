5e448cdbbb6b8fa8a93bca61054661b89e391b0b
edd1977 <mai_den@mail.ru>
2020-06-02 10:01:21 +0300

    first

---
+++ data.js
@@ -0,0 +1,31 @@
+module.exports = function() {
+
+    return {
+
+        users: [
+            {
+                id: 100, name: 'Svetlana Solovieva', balance: 550
+            },
+            {
+                id: 101, name: 'Andrey Dauze', balance: 1050
+            },
+            {
+                id: 101, name: 'Andrey Ivanov', balance: 150
+            },
+            {
+                id: 101, name: 'Denis Petrov', balance: 800
+            },
+            {
+                id: 101, name: 'Olga Solovieva', balance: 350
+            },
+            {
+                id: 101, name: 'Petr Anisimov', balance: 1000
+            },
+            {
+                id: 101, name: 'Alexey Yakov', balance: 445
+            }
+        ]
+
+    }
+
+}
